5c21845feb4ffc28cff183405e846446b176e07c
monu24 <minz.manohar@gmail.com>
2017-02-21 16:27:20 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -20,7 +20,11 @@ import (
 	"errors"
 	"fmt"
 
-	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/hyperledger/fabric/core/chaincode/shim""flag"
+	
+	"log"
+	"net/http"
+	
 )
 
 // SimpleChaincode example simple Chaincode implementation
@@ -32,6 +36,25 @@ func main() {
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
+	// command line flags
+	port := flag.Int("port", 80, "port to serve on")
+	dir := flag.String("directory", "web/", "directory of web files")
+	flag.Parse()
+
+	// handle all requests by serving a file of the same name
+	fs := http.Dir(*dir)
+	fileHandler := http.FileServer(fs)
+	http.Handle("/", fileHandler)
+
+	log.Printf("Running on port %d\n", *port)
+
+	addr := fmt.Sprintf("127.0.0.1:%d", *port)
+	// this call blocks -- the progam runs here forever
+	err := http.ListenAndServe(addr, nil)
+	fmt.Println(err.Error())
+	
+	
+	
 }
 
 // Init resets all the things
