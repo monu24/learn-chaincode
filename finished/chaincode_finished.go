8b16d7a57c2016ae6dd80fbfbea60bd8a907ad2f
monu24 <minz.manohar@gmail.com>
2017-02-23 12:16:25 +0530

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -37,22 +37,7 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string
 	if err != nil {
 		return nil, err
 	}
-	port := flag.Int("port", 80, "port to serve on")
-	dir := flag.String("directory", "web/", "directory of web files")
-	flag.Parse()
-
-	// handle all requests by serving a file of the same name
-	fs := http.Dir(*dir)
-	fileHandler := http.FileServer(fs)
-	http.Handle("/", fileHandler)
-
-	log.Printf("Running on port %d\n", *port)
-
-	addr := fmt.Sprintf("127.0.0.1:%d", *port)
-	// this call blocks -- the progam runs here forever
-	err = http.ListenAndServe(addr, nil)
 	
-	fmt.Println(err.Error())
 
 	return nil, nil
 }
